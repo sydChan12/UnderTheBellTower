4e780079f4aa3ebc7cf33fca18118de5ef2db043
Myra <myrabagga07@gmail.com>
2026-02-08 02:59:14 -0500

    switched path ordering

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+const app = express();
 app.use(express.static(path.join(__dirname, '')));
 const http = require('http');
 const { Server } = require('socket.io');
 
-const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
